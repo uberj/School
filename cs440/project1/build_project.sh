50852361b5e0e0d9579ceae513170dc8f53fcdf4
Eric Zounes <zounes99@gmail.com>
2013-02-10 11:23:05 -0800

    Fixed build script. Running main now.

--- cs440/project1/build_project.sh
+++ cs440/project1/build_project.sh
@@ -7,10 +7,10 @@ then
 	exit 0
 fi
 
-CLASSPATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib/db.jar:. 
-LD_LIBRARY_PATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib
+export CLASSPATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib/db.jar:. 
+export LD_LIBRARY_PATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib
 
 dot_java=`ls *.java`
 javac -classpath $CLASSPATH $dot_java 
-
+java -classpath $CLASSPATH Main 
 
