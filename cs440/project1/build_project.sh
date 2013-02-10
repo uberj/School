faee50fd0b6cf40ac111f9cad8bdc4d3690fd847
Eric Zounes <zounes99@gmail.com>
2013-02-10 11:17:36 -0800

    Added current project directory on classpath.

--- cs440/project1/build_project.sh
+++ cs440/project1/build_project.sh
@@ -7,7 +7,7 @@ then
 	exit 0
 fi
 
-CLASSPATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib/db.jar
+CLASSPATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib/db.jar:. 
 LD_LIBRARY_PATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib
 
 dot_java=`ls *.java`
