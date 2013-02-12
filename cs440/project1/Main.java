76809359c8c36f6b87be4d71adc11942d446fed5
Ian Fridge <enkie1985@gmail.com>
2013-02-11 18:41:04 -0800

    What you want dawg
    This is a commit. lol.

--- cs440/project1/Main.java
+++ cs440/project1/Main.java
@@ -24,6 +24,7 @@ public class Main {
         return b;
     }
 
+
     public static void populateDB() {
         File rootPath = new File("/scratch/cs440/imdb");
         ArrayList<File> paths = new ArrayList<File>();
