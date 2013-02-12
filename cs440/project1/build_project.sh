581ace2f0b99225445694d3dba3ce562b689cf71
Eric Zounes <zounes99@gmail.com>
2013-02-12 10:48:32 -0800

    Added test cases in build script.

--- cs440/project1/build_project.sh
+++ cs440/project1/build_project.sh
@@ -12,5 +12,21 @@ export LD_LIBRARY_PATH=/usr/local/apps/berkeleydb/berkeleydb.5.3/lib
 
 dot_java=`ls *.java`
 javac -classpath $CLASSPATH $dot_java 
-java -classpath $CLASSPATH Main 
+
+if [ "$1" == "test" ]
+then 
+	java -classpath $CLASSPATH Main 1
+	java -classpath $CLASSPATH Main 2 282441.xml
+	java -classpath $CLASSPATH Main 3 20000.xml 30000.xml
+	java -classpath $CLASSPATH Main 4 2000
+	java -classpath $CLASSPATH Main 5 2000 3000
+	java -classpath $CLASSPATH Main 6 20000.xml 30000.xml 2000 3000 
+#	java -classpath $CLASSPATH Main 7 Arnold 
+	exit 0 
+fi
+java -classpath $CLASSPATH Main $args 
+echo "Completed tests." 
+	
+
+	
 
