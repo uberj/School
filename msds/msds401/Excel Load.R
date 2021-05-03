7d29d3eb0055e5d30e30f076e940a2732df08a0e
Jacques Uber <uberj@miradorfin.com>
2021-05-02 17:42:31 -0700

    [master] da 1 done

--- msds/msds401/Excel Load.R
+++ msds/msds401/Excel Load.R
@@ -1,5 +1,5 @@
 install.packages("tidyverse")
 library(readxl)
-BLACK_DATABASES = "Data/black/Databases_in_Excel.xlsx"
+BLACK_DATABASES = "./data/Databases_in_Excel.xlsx"
 excel_sheets(BLACK_DATABASES)
 finance_data = read_excel(BLACK_DATABASES, sheet = "Financial")
